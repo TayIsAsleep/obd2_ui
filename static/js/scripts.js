8da52ba8d4edd08e79cb1270f562c4b972241e88
TayIsAsleep <57579136+TayIsAsleep@users.noreply.github.com>
2023-01-12 13:54:49 +0100

    remove debugging stuff

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -181,11 +181,8 @@ $(document).ready(function(){
                 $("#server_status").text(msg).css("color", "red");
             });
         }else{
-            // set_gauge("all", null);
+            set_gauge("all", null);
         }
-        // if (flag("mobile")){
-        //     $(".main-container").css("background-color","green")
-        // }
     }, loop_delay);
 
     $("#stop-button").click(async function(){
@@ -196,8 +193,4 @@ $(document).ready(function(){
             loop_start();
         }
     });
-
-    $("#test_button").click(function(){
-        set_gauge("all", 100);
-    })
 });
